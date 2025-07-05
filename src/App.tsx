10504ff6b2a9fa5cd3cc579e39717e872b646d90
FelipeDuan <felipeduan1@gmail.com>
2025-07-05 17:16:07 -0300

    feat: create Text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { Text } from "./components/text";
+
 export function App() {
-  return <div className="text-pink-dark">Olá mundo</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <Text variant="body-sm-bold" className="text-pink-base">
+        Olá mundo
+      </Text>
+      <Text className="text-green-base">Olá mundo</Text>
+      <Text variant="body-sm-bold">Olá mundo</Text>
+      <Text>Levar o dog para passear</Text>
+    </div>
+  );
 }
