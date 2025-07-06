35850a8f3e0aa98a2f359989256fc762cdec9a1d
FelipeDuan <felipeduan1@gmail.com>
2025-07-05 22:42:17 -0300

    feat: create Skeleton component

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import { InputText } from "./components/input-text";
 import { InputCheckbox } from "./components/input-checkbox";
 import { Card } from "./components/card";
 import { Container } from "./components/container";
+import { Skeleton } from "./components/skeleton";
 
 export function App() {
   return (
@@ -36,9 +37,10 @@ export function App() {
           <Icon svg={XIcon} className="fill-green-base" />
         </div>
 
-        <div>
+        <div className="flex gap-1">
           <Badge variant="secondary">5</Badge>
           <Badge variant="primary">2 de 5</Badge>
+          <Badge loading>5</Badge>
         </div>
 
         <div>
@@ -49,6 +51,7 @@ export function App() {
           <ButtonIcon icon={TrashIcon} />
           <ButtonIcon icon={TrashIcon} variant={"secondary"} />
           <ButtonIcon icon={TrashIcon} variant={"tertiary"} />
+          <ButtonIcon icon={TrashIcon} loading />
         </div>
 
         <div>
@@ -57,10 +60,17 @@ export function App() {
 
         <div>
           <InputCheckbox />
+          <InputCheckbox loading />
         </div>
 
         <div>
           <Card size={"md"}>Olá mundo</Card>
+        </div>
+
+        <div className="space-y-2">
+          <Skeleton className="h-6" />
+          <Skeleton className="h-6" />
+          <Skeleton className="w-96 h-6" />
         </div>
       </div>
     </Container>
